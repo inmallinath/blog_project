a284461c3b4ea18facf56584fc55cf2d74f88ff2
Mallinath Nagaraj <mallinathin@gmail.com>
2016-03-09 08:29:22 -0800

    Delete app_keys.rb

--- config/initializers/app_keys.rb
+++
@@ -1,4 +0,0 @@
-ENV["email_user_name"] = "answerawesome"
-ENV["email_password"]  = "Sup3r$ecret$$"
-ENV["twitter_consumer_key"] = "V6NOypBewCwHHNwEXR4S4y9Gb"
-ENV["twitter_consumer_secret"] = "Jv9NJHEqfactoQKX3VnvHDQMcF39dg5xM025BMUKc7y3QzTKxo"
