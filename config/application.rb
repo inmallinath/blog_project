c57efa9b39f1a94fb3abfe09d99bb98afb0b5e30
Mallinath Nagaraj <mallinathin@gmail.com>
2016-03-14 19:40:48 -0700

    Assets fun

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module BlogProject
         resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete]
       end
     end
-
+    config.assets.version = "1.1"
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
